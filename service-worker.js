9107e0f2f7316c64c0a9734fbc84a3a97b7d25cb
randhy1109 <45308719+randhy1109@users.noreply.github.com>
2019-10-24 15:53:26 +0700

    Perubahan

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.0.0-rc.0/workbox-sw.js');
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js');
  
 if (workbox)
   console.log(`Workbox berhasil dimuat`);
@@ -16,87 +16,30 @@ else
 
 workbox.routing.registerRoute(
   new RegExp('/pages/'),
-  workbox.strategies.staleWhileRevalidate({
-    cacheName: 'pages'
+ new workbox.strategies.StaleWhileRevalidate({
+    cacheName: 'pages',
   })
 );
 
 workbox.routing.registerRoute(
 'https://private-044be-dicodingfootball.apiary-mock.com/',
-  workbox.strategies.cacheFirst({
-    plugins: [
-      new workbox.expiration.Plugin({
-        maxEntries: 50,
-        maxAgeSeconds: 5 * 60, // 5 minutes
-      }),
-      new workbox.cacheableResponse.Plugin({
-        statuses: [0, 200],
-      }),
-    ],
+new workbox.strategies.CacheFirst({
+  cacheName: 'fecth',
+  plugins: [
+    new workbox.expiration.Plugin({
+      maxEntries: 50,
+      maxAgeSeconds: 5 * 60, // 5 minutes
+    }),
+    new workbox.cacheableResponse.Plugin({
+      statuses: [0, 200],
+    }),
+  ],
 })
+
 );
 
 
-// const CACHE_NAME = "sub-v3";
-// var urlsToCache = [
-//   "/",
-//   "/nav.html",
-//   "/index.html",
-//   "/pages/home.html",
-//   "/pages/daftarLiga.html",
-//   "/pages/liga.html",
-//   "/css/materialize.min.css",
-//   "/js/materialize.min.js",
-//   "/js/nav.js",
-//   "/js/api.js",
-//   "/js/main.js",
-//   "manifest.json",
 
-// ];
- 
-// self.addEventListener("install", function(event) {
-//   event.waitUntil(
-//     caches.open(CACHE_NAME).then(function(cache) {
-//       return cache.addAll(urlsToCache);
-//     })
-//   );
-// });
-
-// self.addEventListener("fetch", function(event) {
-//   var base_url = "https://private-044be-dicodingfootball.apiary-mock.com/";
-
-//   if (event.request.url.indexOf(base_url) > -1) {
-//     event.respondWith(
-//       caches.open(CACHE_NAME).then(function(cache) {
-//         return fetch(event.request).then(function(response) {
-//           cache.put(event.request.url, response.clone());
-//           return response;
-//         })
-//       })
-//     );
-//   } else {
-//     event.respondWith(
-//       caches.match(event.request).then(function(response) {
-//         return response || fetch (event.request);
-//       })
-//     )
-//   }
-// });
-
-// self.addEventListener("activate", function(event) {
-//   event.waitUntil(
-//     caches.keys().then(function(cacheNames) {
-//       return Promise.all(
-//         cacheNames.map(function(cacheName) {
-//           if (cacheName != CACHE_NAME) {
-//             console.log("ServiceWorker: cache " + cacheName + " dihapus");
-//             return caches.delete(cacheName);
-//           }
-//         })
-//       );
-//     })
-//   );
-// });
 
 
 
